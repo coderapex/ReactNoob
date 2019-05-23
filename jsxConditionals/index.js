c57f7c437e52a84905e2195fc77cc5f6bed5d7fc
coderapex <coder.apex@gmail.com>
2019-05-23 05:57:41 +0400

    using conditionals

---
+++ jsxConditionals/index.js
@@ -0,0 +1,17 @@
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+  render() {
+    const num = getNum();
+    return (
+      <div>
+        <h1>The random number is... {num}</h1>
+        <p>{num === 7 ? "Congrats!" : "Unlucky"}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById("root"));
