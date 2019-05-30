911b1c47e7171f9123fa9c4d9ed7071c0a56734f
coderapex <coder.apex@gmail.com>
2019-05-31 01:47:34 +0400

    CSS Concepts

---
+++ 03-cra-intro/src/Dog.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Dog.css";
+class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="Dog">Dog!</h1>
+      </div>
+    );
+  }
+}
+
+export default Dog;
