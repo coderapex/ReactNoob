a36ef9b7d983d3972d8b868d78a79d591b1b4695
coderapex <coder.apex@gmail.com>
2019-06-03 05:23:00 +0400

    Styong and animation on hold

--- 06-dice/src/RollDice.js
+++ 06-dice/src/RollDice.js
@@ -8,7 +8,8 @@ class RollDice extends Component {
 
     this.state = {
       stateA: 0,
-      stateB: 0
+      stateB: 0,
+      isRolling: false
     };
     this.roll = this.roll.bind(this);
   }
@@ -16,13 +17,21 @@ class RollDice extends Component {
   roll = e => {
     let a = Math.floor(Math.random() * 6);
     let b = Math.floor(Math.random() * 6);
-    this.setState({ stateA: a, stateB: b });
+    this.setState({ stateA: a, stateB: b, isRolling: true });
+
+    // add class for wiggle
+
+    // set timeout for 1 sec
+    // after timeout remove wiggle class
   };
   render() {
     return (
       <div className="RollDice">
         <h1>RollDice Component</h1>
-        <Die side={this.state.stateA} />
+        <Die
+          side={this.state.stateA}
+          className={this.state.isRolling ? "dice-wiggle" : ""}
+        />
         <Die side={this.state.stateB} />
         <div className="roll-button">
           <button onClick={this.roll}>Roll Dice!</button>
