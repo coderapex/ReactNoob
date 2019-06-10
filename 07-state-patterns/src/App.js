70a850859a95ba05fb737b6264c8a21e9c2b30b1
coderapex <coder.apex@gmail.com>
2019-06-10 15:07:03 +0400

    Mini Lottery + Styling

--- 07-state-patterns/src/App.js
+++ 07-state-patterns/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import "./App.css";
 import Lottery from "./Lottery";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>State Patterns</h1>
       <Lottery />
+      <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
     </div>
   );
 }
