4d180c902982f9188f5ac66b004ad6e65b87c107
coderapex <coder.apex@gmail.com>
2019-06-16 14:30:03 +0400

    setting up cell flipping

--- 11-lights-out/src/Board.js
+++ 11-lights-out/src/Board.js
@@ -64,6 +64,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log("FLIPPING", coord);
     let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -76,13 +77,21 @@ class Board extends Component {
       }
     }
 
+    // Flip initial cell
+    flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
+
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = false;
 
-    // this.setState({ board, hasWon });
-    this.setState({ board });
+    this.setState({ board: board, hasWon: hasWon });
+    // this.setState({ board });
   }
 
   /** Render game board or winning message. */
@@ -98,7 +107,13 @@ class Board extends Component {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
         let coord = `${y}-${x}`;
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
