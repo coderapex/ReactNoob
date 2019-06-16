e9dbe16ae0e928c6c46963f8a40510a81edfa1e5
coderapex <coder.apex@gmail.com>
2019-06-16 21:53:33 +0400

    form update with computed property names

--- 12-react-forms/src/Form.js
+++ 12-react-forms/src/Form.js
@@ -5,7 +5,9 @@ class Form extends Component {
     super(props);
 
     this.state = {
-      username: ""
+      username: "",
+      email: "",
+      password: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -13,22 +15,47 @@ class Form extends Component {
   }
 
   handleChange = evt => {
-    this.setState({ username: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   handleSubmit = evt => {
     alert(`You typed : ${this.state.username}`);
-    this.setState({ username: "" });
+    this.setState({ username: "", email: "", password: "" });
   };
 
   render() {
     return (
       <div>
-        <h2>FORM</h2>
+        <h2>Form - Single Input</h2>
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
+            name="username"
             value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h2>Form - Multi Input</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.password}
             onChange={this.handleChange}
           />
           <button type="submit">Submit</button>
